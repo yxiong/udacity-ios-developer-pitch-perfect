9a0d27754f750a29919f4aad6eb5652596418848
Ying Xiong <ying@xiongs.org>
2015-09-20 16:57:28 -0700

    Add button to the second screen

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Ying Xiong on 9/20/15.
+//  Copyright © 2015 Ying Xiong. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlowAudio(sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
