fe45cbd5388cecdf57ec4a52b46ca3a7721df53a
Ying Xiong <ying@xiongs.org>
2015-09-20 16:28:03 -0700

    Disable record button and enable stop button when record is in progress

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +23,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        recordButton.enabled = true
+        stopButton.hidden = true
+    }
 
     @IBAction func recordAudio(sender: UIButton) {
+        recordButton.enabled = false
         recordingInProgress.hidden = false
+        stopButton.hidden = false
         // TODO: Record the user's voice
-        print("in recordAudio")
     }
 
     @IBAction func stopRecording(sender: UIButton) {
